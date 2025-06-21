45569a5f898f22b547c265d5fafd88ac38bbec3b
mariaeduardagena <mariaeduardagena@gmail.com>
2025-06-20 22:18:42 -0300

    Create Fibonacci em Vetor - 1176.c

---
+++ Fibonacci em Vetor - 1176.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main() {
+    int n, i, t; 
+    long long fib[61];
+    
+    fib[0] = 0;
+    fib[1] = 1;
+    
+    for(i = 2; i <= 60; i++){
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    
+    scanf("%d", &t);
+    
+    while(t--){
+        scanf("%d", &n);
+        printf("Fib(%d) = %lld\n", n, fib[n]);
+    }
+ 
+    return 0;
+}
