891c364ec1cd4d0732e59a15c0024be9f79844a3
mariaeduardagena <mariaeduardagena@gmail.com>
2025-07-04 14:31:24 -0300

    Coluna na Matriz - 1182.c

--- Coluna na Matriz - 1182.c
+++ Coluna na Matriz - 1182.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
  
 int main() {
-  int coluna, i, j;
-    char operacao;
-    double matriz[12][12], soma = 0.0;
+ int coluna, i, j;
+ char operacao;
+ double matriz[12][12], soma = 0.0;
     
-    scanf("%d", &coluna);
-    getchar(); 
-    scanf("%c", &operacao);
-    
-    for (i = 0; i < 12; i++) {
-        for (j = 0; j < 12; j++) {
-            scanf("%lf", &matriz[i][j]);
+ scanf("%d", &coluna);
+ getchar(); 
+ scanf("%c", &operacao);
+ 
+ for (i = 0; i < 12; i++) {
+  for (j = 0; j < 12; j++) {
+   scanf("%lf", &matriz[i][j]);
         }
     }
     
-    for (i = 0; i < 12; i++) {
-        soma += matriz[i][coluna];
+ for (i = 0; i < 12; i++) {
+  soma += matriz[i][coluna];
     }
     
-    if (operacao == 'S') {
-        printf("%.1lf\n", soma);
+ if (operacao == 'S') {
+  printf("%.1lf\n", soma);
     } else if (operacao == 'M') {
-        printf("%.1lf\n", soma / 12.0);
+  printf("%.1lf\n", soma / 12.0);
     }
-    return 0;
+ return 0;
 }
